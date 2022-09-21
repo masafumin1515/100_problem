02ea7d524343e4d2aaaa5f8f93f905f96f971930
hayashi masafumi <masafumin1515@outlook.jp>
2022-09-22 01:10:51 +0900

    dp_1_2

---
+++ 34.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+
+void _main() {
+  
+}
